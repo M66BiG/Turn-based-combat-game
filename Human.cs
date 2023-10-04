6999e2aca496e58c6b4dc1cc98e3dbec9b61072a
Melih aus Beeck <melihesad@hotmail.de>
2023-10-04 15:07:30 +0200

    Projektdateien hinzufügen.

---
+++ Human.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Turn_based_combat_game
+{
+    internal class Human
+    {
+        public bool headSlotEmpty = true;
+        public bool armorSlotEmpty = true;
+        public bool hand1SlotEmpty = true;
+        public bool hand2SlotEmpty = true;
+    }
+}
