c644e41a96c592e75ae7f1fe54f3692d58fbb38f
Melih aus Beeck <melihesad@hotmail.de>
2023-10-10 02:19:06 +0200

    Added:
    - Hotel System (healing for Payment)
    - Inventory System (equipping items + showing)

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using Turn_based_combat_game;
+﻿using System.Diagnostics.Metrics;
+using Turn_based_combat_game;
 
 internal class Program
 {
@@ -22,7 +23,7 @@ internal class Program
     public static void ShowNavigation(Human Spieler)
     {
         Console.WriteLine("Du befindest dich im Hauptmenü");
-        Console.WriteLine("1: zum Haus");
+        Console.WriteLine("1: zum Hotel");
         Console.WriteLine("2: zum Shop");
         Console.WriteLine("3: zum Inventar");
         Console.WriteLine("4: zum Kampf");
@@ -50,16 +51,48 @@ internal class Program
             case 4:
                 ShowFight(Spieler);
                 break;
-
-
-
             default:
                 ShowNavigation(Spieler);
                 break;
         }
     }
+    //-------------------------------------------------------------------
+    // Home Home Home Home Home Home Home Home Home Home Home Home Home 
+    //-------------------------------------------------------------------
+
+    public static void ShowHome(Human Spieler)
+    {
+        Console.WriteLine("Du bist jetzt in einem Hotel. Möchtest du ein Zimmer buchen? Kostet 10 Gold die Nacht.");
+        Console.WriteLine("Y/N");
+
+        string answer = Console.ReadLine();
+        if (answer.ToUpper() == "Y")
+        {
+            Spieler.Gold -= 10;
+            Console.WriteLine($"Du hast nurnoch {Spieler.Gold}");
+            if (Spieler.Hp <= 80 && Spieler.Hp != 0)
+            {
+                Spieler.Hp += 20;
+                Console.WriteLine("Du hast dich um 20 HP geheilt.");
+                Console.WriteLine($"Du hast jetzt {Spieler.Hp} HP");
+            }
+            else
+            {
+                Console.WriteLine($"Du hast dich um {100 - Spieler.Hp} HP geheilt.");
+                Spieler.Hp = Spieler.Hp + (100 - Spieler.Hp);
+                Console.WriteLine($"Du hast jetzt {Spieler.Hp} HP");
+            }
 
 
+
+        }
+        else if (answer.ToUpper() != "N")
+        {
+            Console.WriteLine("Unbekannte Eingabe.");
+        }
+        Console.WriteLine("Kehre zum Hauptmenü.");
+        ShowNavigation(Spieler);
+    }
     //-------------------------------------------------------------------
     // Shop Shop Shop Shop Shop Shop Shop Shop Shop Shop Shop Shop Shop 
     //-------------------------------------------------------------------
@@ -82,13 +115,13 @@ internal class Program
         Console.WriteLine($"{counter}: Verlassen");
 
 
-        // Hierunter wird gecheckt ob genug Gold vorhanden ist.
+        // Hierdrunter wird gecheckt ob genug Gold vorhanden ist.
 
 
         int input = ShopAction(Convert.ToInt32(Console.ReadLine()), counter, Spieler);
 
         if (input != 0)
-        {   // itemStats(Klasse) .Stats (Dictionary) [temp (Dictionary mit string speicher) [input]] (Nummer) .Cost (Kosten des Items)
+        {   // itemStats(Klasse) .Stats (Dictionary) [temp (Dictionary mit string speicher) [input]] (Nummer (key)) .Cost (Kosten des Items)
             if (Spieler.Gold >= itemStats.Stats[temp[input]].Cost ) 
             {
                 Console.WriteLine($"Du hast {Spieler.Gold} Gold.");
@@ -147,18 +180,47 @@ internal class Program
     //-------------------------------------------------------------------
     public static void ShowInventoryList(Human Spieler)
     {
+        Console.WriteLine("Du schaust in dein Inventar.");
 
+
+        if (Spieler.Inv.Count != 0)
+        {
+            Dictionary<int, string> temp = new Dictionary<int, string>();
+            int counter = 1;
+            foreach (string item in Spieler.Inv)
+            {
+                Console.WriteLine($"{counter}: {item}");
+                temp.Add(counter++, item);
+            }
+            Console.WriteLine("Möchtest du etwas anziehen?");
+            string answer = Console.ReadLine();
+            if (answer.ToUpper() == "Y")
+            {
+                Console.WriteLine("Welches Item?");
+                int input = Convert.ToInt32(Console.ReadLine());
+                Spieler.Equipt.Add(temp[input]);
+                Spieler.Inv.Remove(temp[input]);
+
+                Console.WriteLine($"Du hast {temp[input]} angezogen.");
+            }
+            else if (answer.ToUpper() == "N")
+            {
+
+            }
+            else
+            {
+
+            }
+        }
+        else
+        {
+            Console.WriteLine("Du hast keine Items.");
+            Console.WriteLine("Kehre zurück zum Hauptmenü.");
+
+
+        }
+        ShowNavigation(Spieler);
     }
-
-
-    //-------------------------------------------------------------------
-    // Home Home Home Home Home Home Home Home Home Home Home Home Home 
-    //-------------------------------------------------------------------
-    public static void ShowHome(Human Spieler)
-    {
-
-    }
-
 
     //-------------------------------------------------------------------
     // Fight Fight Fight Fight Fight Fight Fight Fight Fight Fight Fight 
