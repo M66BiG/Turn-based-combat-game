41c81172aa43ebf660e94c1333a528e6d80a797f
Q-Sharp <8755388+Q-Sharp@users.noreply.github.com>
2023-10-13 13:18:40 +0200

    place project dir INSIDE the git repo

---
+++ TurnBasedCombat.Shared/Entitites/Item.cs
@@ -0,0 +1,11 @@
+﻿namespace TurnBasedCombat.Shared.Entitites;
+
+public class Item
+{
+    public static Item None => new(); 
+
+    public int Cost { get; set; }
+    public int Damage { get; set; }
+    public int Armor { get; set; }
+    public Slot Slot { get; set; }
+}
---
+++ TurnBasedCombat.Shared/Entitites/ItemStats.cs
@@ -0,0 +1,18 @@
+﻿namespace TurnBasedCombat.Shared.Entitites;
+
+public class ItemStats
+{
+    public Dictionary<string, Item> Stats = new()
+    {
+        {"Kleinschwert",        new Item{Cost = 10, Damage = 5, Armor = 0, Slot = Slot.OneHand } },
+        {"Zweihand Schwert",    new Item{Cost = 40, Damage = 25, Armor = 0, Slot = Slot.TwoHand } },
+
+        {"Einhand Schild",      new Item{Cost = 10, Damage = 2, Armor = 10, Slot = Slot.OffHand } },
+
+        {"Leichter Helm",       new Item{Cost = 10, Damage = 0, Armor = 5, Slot = Slot.HeadArmor } },
+
+        {"Leichte Rüstung",     new Item{Cost = 10, Damage = 0, Armor = 10, Slot = Slot.BodyArmor } },
+        {"Mittelere Rüstung",   new Item{Cost = 20, Damage = 0, Armor = 20, Slot = Slot.BodyArmor } },
+        {"Schwere Rüstung",     new Item{Cost = 40, Damage = 0, Armor = 40, Slot = Slot.BodyArmor } }
+    };
+}
